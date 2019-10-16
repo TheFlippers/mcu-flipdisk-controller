5ab62e80d2183ae201ed4c082a9eb95398c98ba8
nate <nate.givens03@gmail.com>
2019-10-15 22:31:08 -0400

    feat: detect dma transfers and keep previous and next data

--- inc/main.h
+++ inc/main.h
@@ -1,2 +1,4 @@
+#include<string.h>
+
 #include "fdd_spi.h"
 #include "fdd_display.h"
--- src/main.c
+++ src/main.c
@@ -1,23 +1,36 @@
 /**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
-*/
+ ******************************************************************************
+ * @file    main.c
+ * @author  Ac6
+ * @version V1.0
+ * @date    01-December-2013
+ * @brief   Default main function.
+ ******************************************************************************
+ */
 
 
 #include "stm32f4xx.h"
 #include "main.h"
 
-int main(void)
-{
+int main(void) {
+  // XXX first neighbor byte is self id: change this for different display
+  // controllers
+  // TODO fix flow to process first byte from pi
+  uint8_t neighbors[5] = { 1, 0 };
+
   uint8_t pixels[8] = { 0 };
-  uint8_t neighbors[5] = { DISPLAY_ID, 0 };
+  uint8_t prev[7] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+  uint8_t next[7] = { 0 };
 
   FDDdisplay_init();
-  FDDdisplay_spi_init(pixels, neighbors);
-	for(;;);
+  FDDspi_init(pixels, neighbors);
+
+  for(;;) {
+    if(DMA1->LISR & DMA_LISR_TCIF3) {
+      memcpy(next, &pixels[1], 7);
+      FDDdisplay_draw(prev, next);
+      DMA1->LIFCR = DMA_LIFCR_CTCIF3;
+      memcpy(prev, next, 7);
+    }
+  }
 }
